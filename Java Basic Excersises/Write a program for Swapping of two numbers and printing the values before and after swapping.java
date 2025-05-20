f1b8b3b5ba7c72aa9132040a70ea7f473dc8b963
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-20 14:33:15 +0530

    Add files via upload
    Write a program for swapping of two numbers before and after swapping their values .

---
+++ Java Basic Excersises/Write a program for Swapping of two numbers and printing the values before and after swapping.java
@@ -0,0 +1,19 @@
+/* Write a Program for Swapping of two numbers and printing the value before and after Swapping  */
+
+import java.util.Scanner;
+
+class Exe6 {
+   Exe6() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the first value :");
+      double var2 = var1.nextDouble();
+      System.out.println("Enter the second value :");
+      double var4 = var1.nextDouble();
+      System.out.println("After Swapping:");
+      System.out.println("Value of first number :" + var4);
+      System.out.println("Value of second number :" + var2);
+   }
+}
