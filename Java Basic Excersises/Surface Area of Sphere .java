c8015e28bedec6ded85c7535f121d6f63b1f9f42
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-21 15:54:46 +0530

    Add files via upload
    Write a Program to calculate Surface Area of Sphere .

---
+++ Java Basic Excersises/Surface Area of Sphere .java
@@ -0,0 +1,19 @@
+// Write a Program to calculate Surface Area of Sphere :-
+
+// Surface Area of Sphere :-
+
+
+import java.util.Scanner;
+
+class Exe14 {
+   Exe14() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the value of radius :");
+      double var2 = var1.nextDouble();
+      double var4 = 12.56 * var2 * var2;
+      System.out.println("The surface area of sphere is : " + var4);
+   }
+}
